093da0eeb6f4a8d97807e85d94c1203c8cf93669
Administrador <Administrador@10.7.19.191>
2015-08-19 19:18:25 -0500

    prueba taller

---
+++ Productioncj/src/productioncj/Prueba.java
@@ -0,0 +1,10 @@
+package productioncj;
+
+// public class Prueba {  public static void main (String[] args) {   int acum = 0;   int i = 0;   while ( i < 5 ) {     acum = acum + i;     i = i + 2;   }   System.out.println(""+acum);      } }
+//public class Prueba {  public static void main (String[] args) {   int total = 3 % 5;   int valor = 2;   while ( total > valor ) {     total += valor;   }   System.out.println(""+total);      } } 
+//public class Prueba {  public static void main (String[] args) {   int x = 12;   int y = 3;    int total = 0;   while ( total < x ) {     total += y;     System.out.println(""+total);   }      } }
+//public class Prueba {  public static void main (String[] args) {   int deposito = 200;   int acum = 1000;   while ( deposito < acum ) {     deposito += 2*deposito;   }   System.out.println(""+acum);      } }
+//public class Prueba {    public static void main (String[] args) {   int b = 6;   for (int i = b; i < b; i--) {    b--;   }    System.out.println(""+ b);      } } 
+//public class Prueba {    public static void main (String[] args) {   int a, b;   a = 12;   b = 3;   for (int i = 1; i < b; i++) {    a += b;   }     System.out.println(""+a);      } } 
+public class Prueba {    public static void main (String[] args) {   int acumulado = 0;   for (int i = 2 ; i < 12; i++) {    acumulado += i++;   }    System.out.println(""+ acumulado);     } } 
+
---
+++ Productioncj/src/productioncj/dias.java
@@ -0,0 +1,53 @@
+package productioncj;
+
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+public class dias extends Applet implements ActionListener {
+    Label l1,l2,l3,l4,l5;
+
+    public TextField getT5() {
+        return t5;
+    }
+
+    public void setT5(TextField t5) {
+        this.t5 = t5;
+    }
+    TextField t1,t2,t3,t4,t5;
+    Button b;
+    int anon = 365;
+    int anob = 366;
+    
+    public void dias(){
+    
+    l1 = new Label("Año");
+    t1 = new TextField(1);
+    l2 = new Label("Dias");
+    t2 = new TextField(2);
+    l3 = new Label("Año");
+    t3 = new TextField(3);
+    l4 = new Label("Mes");
+    t4 = new TextField(4);
+    l5 = new Label("Dias");
+    t5 = new TextField(5);
+    b = new Button("Calcular");
+    add(l1);
+    add(t1);
+    add(l2);
+    add(t2);
+    add(b);
+    add(l3);
+    add(t3);
+    add(l4);
+    add(t4);
+    add(l5);
+    add(t5);
+    b.addActionListener(this);
+}
+    public void actionPerformed(ActionEvent ae){
+        int ano = Integer.parseInt(t1.getText());
+        int mes = Integer.parseInt(t2.getText());
+        
+    }
+}
